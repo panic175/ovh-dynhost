68f7f82b395a7eb6ee3110f0a3a905b8093a4093
Pierre Macherel <noreply@github.com>
2019-10-14 22:31:07 +0200

    better logs

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,13 @@ WORKDIR /usr/src/app
 COPY dynhost.sh .
 RUN chmod +x ./dynhost.sh
 
+ENV STDOUT=/usr/src/app/dynhost.log \
+    STDERR=/usr/src/app/dynhost.err
+
+# forward request and error logs to docker log collector
+RUN ln -sf /dev/stdout $STDOUT \
+	&& ln -sf /dev/stderr $STDERR
+
 RUN ln -sf /usr/src/app/dynhost.sh /etc/periodic/15min/dynhost
 
-CMD ["crond", "-f"]
+CMD [ "crond", "-f" ]
--- dynhost.sh
+++ dynhost.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-
-echo "Checking IP" >> dynhost.log
-
-IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d \")
-CURRENT_IP=$(dig +short $DYNHOST_DOMAIN_NAMES)
-
-if [ "$IP" ]; then
-    if [ "$CURRENT_IP" != "$IP" ]; then
+{
+    IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d \")
+    if [ "$IP" ]; then
         for DOMAIN_NAME in ${DYNHOST_DOMAIN_NAMES}; do
-            echo "Updating $DOMAIN_NAME from $CURRENT_IP to $IP" >> dynhost.log
-            curl --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}" "https://www.ovh.com/nic/update?system=dyndns&hostname=${DOMAIN_NAME}&myip=${IP}" >> dynhost.log 2>&1
+            CURRENT_IP=$(dig +short $DOMAIN_NAME)
+            if [ "$CURRENT_IP" != "$IP" ]; then
+                    echo "Updating $DOMAIN_NAME from $CURRENT_IP to $IP"
+                    curl -s --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}s" \
+                        "https://www.ovh.com/nic/update?system=dyndns&hostname=${DOMAIN_NAME}&myip=${IP}" \
+                        | grep --color=never "title" >&2
+            fi
         done
     else
-        echo "No update required" >> dynhost.log
+        >&2 echo "IP not found"
     fi
-else
-    echo "IP not found" >> dynhost.log
-fi
+} >>$STDOUT 2>>$STDERR
